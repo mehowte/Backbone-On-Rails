3b1a0c6407548e20bc32ac550025e2421574ac7d
Michał Taszycki <mtaszycki@gmail.com>
2011-08-01 14:34:51 +0300

    apply javascript variable naming convention

--- backbone_on_rails.js
+++ backbone_on_rails.js
@@ -1,32 +1,32 @@
-Backbone.Model.prototype.old_parse = Backbone.Model.prototype.parse
+Backbone.Model.prototype.oldParse = Backbone.Model.prototype.parse
 Backbone.Model.prototype.parse = function() {
-  var params = this.old_parse.apply(this, arguments);
+  var params = this.oldParse.apply(this, arguments);
   var result = {}
-  if (this.params_namespace)
-    result = params[this.params_namespace];
+  if (this.paramsNamespace)
+    result = params[this.paramsNamespace];
   else
     result = params;
   return result;
 }
 
-Backbone.Model.prototype.old_toJSON = Backbone.Model.prototype.toJSON
+Backbone.Model.prototype.oldToJSON = Backbone.Model.prototype.toJSON
 Backbone.Model.prototype.toJSON = function() {
-  var json = this.old_toJSON();
+  var json = this.oldToJSON();
   var result = {}
-  if (this.params_namespace)
-    result[this.params_namespace] = json;
+  if (this.paramsNamespace)
+    result[this.paramsNamespace] = json;
   else
     result = json;
   return result;
 }
 
-Backbone.Collection.prototype.old_parse = Backbone.Collection.prototype.parse
+Backbone.Collection.prototype.oldParse = Backbone.Collection.prototype.parse
 Backbone.Collection.prototype.parse = function() {
-  var params = this.old_parse.apply(this, arguments);
+  var params = this.oldParse.apply(this, arguments);
   var collection = this;
-  if (this.params_namespace) {
+  if (this.paramsNamespace) {
     var result = []
-    $(params).each(function(){result.push(this[collection.params_namespace])})
+    $(params).each(function(){result.push(this[collection.paramsNamespace])})
     return result;
   } else
     return params;
